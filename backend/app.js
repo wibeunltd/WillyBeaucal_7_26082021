900418689f951a394200a8b407ccb64c96d3582c
wibeunltd <wibe@hotmail.fr>
2021-08-26 17:52:03 +0200

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+// Module requis
+const express   = require('express')
+const morgan    = require('morgan')
+const helmet    = require('helmet')
+const cors      = require('cors')
+const path      = require('path')
+const favicon   = require('serve-favicon')
+
+// Variables d'environnement
+require('dotenv').config()
+
+// Instanciation de l'application
+const app = express()
+
+app
+.use(express.json())
+.use(express.urlencoded({ extended: true }))
+.use(morgan('dev'))
+.use(helmet())
+.use(cors())
+.use(express.static(path.join(__dirname, '/public/')))
+.use('/', require('./routes/mainRoutes'))
+
+// Requêtes CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGINS)
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+})
+
+module.exports = app
