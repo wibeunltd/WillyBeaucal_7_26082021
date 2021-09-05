259150a9cf8c61446b8c6fb3a1809ed11c4d3d85
wibeunltd <wibe@hotmail.fr>
2021-09-06 00:17:34 +0200

    mise à jour mailer.js
    - Correction minime

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -15,7 +15,7 @@ const transporter = nodemailer.createTransport({
 })
 
 module.exports = {
-    async registerActivationMail(email, firstname, createdAt, registerId) {
+    async registerActivationMail(email, firstName, createdAt, registerId) {
         const activationLink = `${process.env.BASE_URL}api/users/activation/${email}/${registerId}`;
 
         const msg = await transporter.sendMail({
@@ -23,7 +23,7 @@ module.exports = {
             to: email,
             subject:"Moments ● Merci d'activer votre compte",
             html:
-            `<style type="text/css">@import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap);body{font-size:12px;}h1{font-size:16px}.button{background-color:#4caf50;border:none;border-radius:20px;color:#fff;padding:15px 32px;text-align:center;text-decoration:none;display:inline-block}.info{font-size:9px}</style><h1>Hello ${firstname} 😀,</h1><p>Nous sommes heureux, que vous vous soyez inscrit à <strong>Moments</strong>. Complétez votre inscription en cliquant sur le bouton ci-dessous. Ce lien vérifiera votre adresse e-mail, puis vous ferez officiellement partie de la communauté.</p><a class="button" href="${activationLink}" target="_blank">Vérifier votre email</a><p></p><p>Si vous rencontrez des problèmes avec le bouton de vérification, veuillez coller l'URL suivante dans votre navigateur Web :</p><p>${activationLink}</p><p>Bien cordialement, l'équipe Moments</p><p class="info">Ceci est un mail automatique, suite à votre inscription du ${createdAt}, merci de ne pas y répondre. Si vous n'êtes pas à l'origine de cette demande d'inscription, merci d'ignorer ce mail. <strong>Le lien de vérification expire dans 1 heure.</strong></p>`
+            `<style type="text/css">@import url(https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap);body{font-size:12px;}h1{font-size:16px}.button{background-color:#4caf50;border:none;border-radius:20px;color:#fff;padding:15px 32px;text-align:center;text-decoration:none;display:inline-block}.info{font-size:9px}</style><h1>Hello ${firstName} 😀,</h1><p>Nous sommes heureux, que vous vous soyez inscrit à <strong>Moments</strong>. Complétez votre inscription en cliquant sur le bouton ci-dessous. Ce lien vérifiera votre adresse e-mail, puis vous ferez officiellement partie de la communauté.</p><a class="button" href="${activationLink}" target="_blank">Vérifier votre email</a><p></p><p>Si vous rencontrez des problèmes avec le bouton de vérification, veuillez coller l'URL suivante dans votre navigateur Web :</p><p>${activationLink}</p><p>Bien cordialement, l'équipe Moments</p><p class="info">Ceci est un mail automatique, suite à votre inscription du ${createdAt}, merci de ne pas y répondre. Si vous n'êtes pas à l'origine de cette demande d'inscription, merci d'ignorer ce mail. <strong>Le lien de vérification expire dans 1 heure.</strong></p>`
         });
         
         console.log("Message sent: %s", msg.messageId, 'Preview URL: %s', nodemailer.getTestMessageUrl(msg))
