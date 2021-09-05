f60794b4273261ed03e58b81028fe84914c98d64
wibeunltd <wibe@hotmail.fr>
2021-09-05 14:21:54 +0200

    mise à jour mainCtrl.js

--- backend/controllers/mainCtrl.js
+++ backend/controllers/mainCtrl.js
@@ -2,5 +2,5 @@
 require('dotenv').config()
 
 exports.init = (req, res) => {
-    res.json(`Backend du réseau social Moments de GROUPOMANIA. Make with ❤ by ${process.env.OWNER} !`)
+    res.json(`Backend du réseau social Moments from GROUPOMANIA. Make with ❤ by ${process.env.OWNER} !`)
 }
