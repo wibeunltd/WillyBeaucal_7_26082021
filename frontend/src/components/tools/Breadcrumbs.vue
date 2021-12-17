ae01ca4b0272121c479b38766130929c3f43c023
wibeunltd <wibe@hotmail.fr>
2021-12-17 19:17:46 +0100

    Création fil d'ariane

---
+++ frontend/src/components/tools/Breadcrumbs.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="py-2">
+        <ol class="breadcrumb">
+             <li v-for="(breadcrumb, index) in breadcrumbList" :key="index" :class="{active : breadcrumb.active}" class="breadcrumb-item"><a :href="breadcrumb.link">{{ breadcrumb.name }}</a></li>
+        </ol>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Beadcrumb',
+    data() {
+        return {
+            breadcrumbList: this.$route.meta.breadcrumb,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.breadcrumb {
+    margin-bottom: 0;
+}
+</style>
