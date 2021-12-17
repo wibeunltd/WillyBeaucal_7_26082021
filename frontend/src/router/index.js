ab2bc6c4b76c529017bffdc5031c9864c3bde131
wibeunltd <wibe@hotmail.fr>
2021-12-17 18:58:48 +0100

    Màj index.js
    Route accueil
    Gestion des titres des pages
    Redirection en fonction du statut de l'utilisateur

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from "../store"
 
 const routes = [
-  
-  
+  {
+    path:'/',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
+    meta: {
+      title: 'Réseau social interne par Groupomania ● Moments',
+      breadcrumb: [{ name: 'Accueil', active: true }],
+      guest: true
+    }
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// Gestion des titres de pages
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next()
+});
+
+// Redirection des utilisateurs authentifiés pour les routes accessibles en tant qu'invité
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.guest)) {
+    if (store.getters.isLoggedIn) {
+      next("/feed")
+      return
+    }
+    next()
+  } else {
+    next()
+  }
+})
+
+// Redirection des utilisateurs non authentifiés pour les routes accessibles en tant que membre
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next("/login")
+  } else {
+    next()
+  }
 })
 
 export default router
