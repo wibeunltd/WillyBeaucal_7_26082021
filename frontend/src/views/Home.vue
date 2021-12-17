14d56bb12acacc0199e7dc02ee75859008c58c23
wibeunltd <wibe@hotmail.fr>
2021-12-17 18:57:01 +0100

    Création Home.vue
    Page d'accueil

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- <NavBar /> -->
+    <Heroes />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// import NavBar from '@/components/home/NavBar.vue'
+import Heroes from '@/components/home/Heroes.vue'
+import Footer from '@/components/home/Footer.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+  components: {
+    // NavBar,
+    Heroes,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-family: 'Volkhov', serif;
+  font-weight: 700;
+  font-size: 3.125rem;
+  color: #181E4B;
+  margin: 0;
+}
+
+.author {
+  font-style: italic;
+  font-weight: 300;
+  color: #181E4B;
+}
+</style>
