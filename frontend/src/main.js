7122067741974b0aee5bc71dfa717ceb882a3d8a
wibeunltd <wibe@hotmail.fr>
2021-12-17 18:57:57 +0100

    Màj main.js
    Import des dépendances
    Configuration des dépendances

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,30 @@
+// Imports
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import Toast from 'vue-toastification'
+import moment from 'moment'
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@/assets/css/main.css"
+import "vue-toastification/dist/index.css"
 
-createApp(App).use(store).use(router).mount('#app')
+// Instanciation de l'application
+const app = createApp(App)
+
+// Déclaration de l'entête d'authentification
+axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
+
+// Configuration globale de moment
+app.config.globalProperties.$moment = moment
+moment.locale('fr')
+
+app
+.use(store)
+.use(router)
+.use(Toast, {
+    transition: "Vue-Toastification__fade"
+})
+.mount('#app')
