e6e2134d37d0c752edce2a828b3c6f8d50dfcbdf
wibeunltd <wibe@hotmail.fr>
2021-12-18 13:35:58 +0100

    Màj Footer.vue

--- frontend/src/components/home/Footer.vue
+++ frontend/src/components/home/Footer.vue
@@ -25,12 +25,12 @@
             </div>
         </div>
         <div class="d-flex justify-content-between pt-4 mt-4 border-top">
-            <p>&copy; {{ copyright }} Groupomania &middot; Inc. Tous droits réservés.</p>
+            <p>&copy; {{ copyright }} &middot; Inc. Tous droits réservés.</p>
             <p>Made with
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill love" viewBox="0 0 16 16">
                     <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
                 </svg>
-                in France.
+                by an <a class="oc" :href="urlOC">OpenClassrooms</a> <a class="text-dark" :href="studentGitUrl" :title="studentIdentity">student</a>.
             </p>
         </div>
     </footer>
@@ -40,10 +40,18 @@
 <script>
 export default {
     name: 'Footer',
+    data() {
+        return {
+            companyName: 'Groupomania',
+            studentGitUrl: 'https://github.com/wibeunltd',
+            studentIdentity: 'Willy Beaucal',
+            urlOC: 'https://openclassrooms.com/fr/'
+        }
+    },
     computed: {
         copyright() {
             const currentYear = new Date().getFullYear()
-            return `${currentYear}`
+            return `${currentYear} ${this.companyName}`
         }
     }
 }
@@ -64,5 +72,9 @@ export default {
     0%{transform:scale(1)}
     50%{transform:scale(1.3)}
     100%{transform:scale(1)}
+}
+
+.oc{
+    color: #7451eb;
 }
 </style>
