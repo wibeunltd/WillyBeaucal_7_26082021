e62ac5f8a591fa714622cb82308db246a12ada94
wibeunltd <wibe@hotmail.fr>
2021-12-19 12:33:47 +0100

    Màj
    Correction et ajout tooltip

--- frontend/src/components/home/Footer.vue
+++ frontend/src/components/home/Footer.vue
@@ -30,7 +30,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill love" viewBox="0 0 16 16">
                     <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
                 </svg>
-                by an <a class="oc" :href="urlOC">OpenClassrooms</a> <a class="text-dark" :href="studentGitUrl" :title="studentIdentity">student</a>.
+                by an <a class="oc" :href="urlOC">OpenClassrooms</a> <a class="text-dark" :href="studentGitUrl"><VueCustomTooltip :label="studentIdentity">student</VueCustomTooltip></a>.
             </p>
         </div>
     </footer>
@@ -44,7 +44,7 @@ export default {
         return {
             companyName: 'Groupomania',
             studentGitUrl: 'https://github.com/wibeunltd',
-            studentIdentity: 'Willy Beaucal',
+            studentIdentity: 'Willy BEAUCAL',
             urlOC: 'https://openclassrooms.com/fr/'
         }
     },
