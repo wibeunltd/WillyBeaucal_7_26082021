353043f85a199e2554697f24b5766a1caaaee943
wibeunltd <wibe@hotmail.fr>
2022-01-14 17:57:59 +0100

    Création CookieBanner.vue
    Alerte sur l'utilisation des cookies (RGPD) simplifiée

---
+++ frontend/src/components/tools/CookieBanner.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="alert alert-primary text-center fixed-bottom mb-0 alert-dismissible fade show" role="alert" :class="display">
+    Ce site utilise des cookies pour vous fournir la meilleure expérience de navigation possible. En continuant sur ce site, vous acceptez l'utilisation des cookies.
+    <button type="button" class="btn btn-primary btn-sm ms-3 close" data-bs-dismiss="alert" aria-label="Close" @click.prevent="hideCookie">Accepter</button>
+  </div>
+</template>
+
+<script>
+import Cookies from 'js-cookie'
+export default {
+  name: 'CookieBanner',
+  data() {
+    return {
+      display: '',
+    }
+  },
+  created() {
+    if (Cookies.get('moments') === undefined || Cookies.get('moments') === 'rejected') {
+      Cookies.set('moments', 'rejected', { sameSite: 'strict'})
+      this.display = 'd-block'
+    } else {
+      this.display = 'd-none'
+    }
+  },
+  methods: {
+    hideCookie() {
+      Cookies.set('moments', 'accepted', { sameSite: 'strict'})
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
